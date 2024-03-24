72abdf1dd7777aa4c93d0a0482523cdacf145240
AdharRag1 <adharsebei88@gmail.com>
2024-03-24 19:50:24 +0100

    html-ccs-js

---
+++ video preview/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+/*var x = document.createElement("myVideo");*/
+
+function over(element) {
+    myVideo.play();  
+}
+                                
+function out(element) {
+    myVideo.pause();   
+}
+
